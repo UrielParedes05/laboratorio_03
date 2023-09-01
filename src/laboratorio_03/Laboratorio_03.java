3a63ef1032d65316abb8acaee0c5b78290cd7236
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-09-01 14:16:07 -0500

    Add files via upload

---
+++ src/laboratorio_03/Laboratorio_03.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_03;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Laboratorio_03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ListaPersonas lista = new ListaPersonas();
+        
+        lista.setVisible(true);
+        lista.setLocationRelativeTo(null);
+    }
+    
+}
